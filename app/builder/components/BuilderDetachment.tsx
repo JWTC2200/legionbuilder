0fb93ee381c6ed69f1cc6d1b9fa96aa8b85386b2
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-11-23 13:25:40 +0000

    fixes

--- app/builder/components/BuilderDetachment.tsx
+++ app/builder/components/BuilderDetachment.tsx
@@ -17,12 +17,12 @@ import { detachmentPoints, detachmentSize } from "../utils";
 import { getDetachmentIcons } from "@/app/utils/detachmentIcons";
 
 const BuilderDetachment = ({
-  slot,
+  detachmentSlot,
   faction,
   slotSet,
   setArmyList,
 }: {
-  slot: BUILDER_DETACHMENT_SLOT;
+  detachmentSlot: BUILDER_DETACHMENT_SLOT;
   faction: FACTION | null;
   slotSet: SLOTSET;
   setArmyList: React.Dispatch<React.SetStateAction<BUILDER_LIST>>;
@@ -30,24 +30,29 @@ const BuilderDetachment = ({
   // const [slotState, setSlotState] = useState(slot);
 
   // filtering for custom detachment slots
-  const detachmentOptions: DETACHMENT[] = slot.restricted
-    ? slot.options.map(
+  const detachmentOptions: DETACHMENT[] = detachmentSlot.restricted
+    ? detachmentSlot.options.map(
         (option) =>
           detachmentData.filter((detachment) => detachment.id === option)[0]
       )
     : detachmentData
         .filter(
           (detachment) =>
-            detachment.detachment_type === slot.type &&
+            detachment.detachment_type === detachmentSlot.type &&
             detachment.faction === faction &&
             !detachment.unique
         )
         .concat(
-          detachmentData.filter((detach) => slot.options?.includes(detach.id))
+          detachmentData.filter((detach) =>
+            detachmentSlot.options?.includes(detach.id)
+          )
         );
 
   const selectOptions = detachmentOptions.map((option, index) => (
-    <option key={slot.slot_ref + "unitOption" + index} value={option.id}>
+    <option
+      key={detachmentSlot.slot_ref + "unitOption" + index}
+      value={option.id}
+    >
       {option.name}: {option.base_cost}pts
     </option>
   ));
@@ -147,7 +152,7 @@ const BuilderDetachment = ({
     newUnit: BUILDER_DETACHMENT_UNIT | null
   ): BUILDER_DETACHMENT_SLOT[] => {
     const newArray = detachmentArray.map((detach) => {
-      if (detach.slot_ref === slot.slot_ref) {
+      if (detach.slot_ref === detachmentSlot.slot_ref) {
         return { ...detach, selected_unit: newUnit };
       }
       return detach;
@@ -163,7 +168,7 @@ const BuilderDetachment = ({
         const newArmyList = {
           ...prev,
           formations: prev.formations.map((form) => {
-            if (form.ref_id === slot.ref_id) {
+            if (form.ref_id === detachmentSlot.ref_id) {
               return {
                 ...form,
                 [slotSet]: form[slotSet]
@@ -184,31 +189,32 @@ const BuilderDetachment = ({
   return (
     <div className="sm:border-2 flex-grow border-black flex flex-col max-w-md">
       <h4 className="w-full flex gap-1 items-center justify-center bg-green-950 text-green-50 text-graduate text-center py-1 sm:text-xl font-graduate px-2">
-        {getDetachmentIcons(slot.type)} {slot.type}
-        {getDetachmentIcons(slot.type)}
+        {getDetachmentIcons(detachmentSlot.type)} {detachmentSlot.type}
+        {getDetachmentIcons(detachmentSlot.type)}
       </h4>
-
+      <button onClick={() => console.log(detachmentSlot)}>TEST</button>
       {/* DESCRIPTION  */}
-      {slot.description ? (
-        <p className="italic text-center">*{slot.description}*</p>
+      {detachmentSlot.description ? (
+        <p className="italic text-center">*{detachmentSlot.description}*</p>
       ) : null}
 
       {/* UNIT POINTS AND DETACHMENT SIZE */}
-      {slot.selected_unit ? (
+      {detachmentSlot.selected_unit ? (
         <div className="w-full flex flex-wrap gap-2 sm:gap-4 justify-center items-center px-1">
           <p className="text-sm sm:text-base font-graduate font-bold">
-            Detachment size:{detachmentSize(slot.selected_unit)}
+            Detachment size:{detachmentSize(detachmentSlot.selected_unit)}
           </p>
           <p className="sm:text-sm font-graduate font-bold border-b underline">
-            {detachmentPoints(slot.selected_unit)}
+            {detachmentPoints(detachmentSlot.selected_unit)}
             <span className="text-base font-normal ">points</span>
           </p>
         </div>
       ) : null}
 
       {/* UNIT OVER DETACHMENT SIZE WARNING */}
-      {slot.selected_unit ? (
-        detachmentSize(slot.selected_unit) > slot.selected_unit.max_size ? (
+      {detachmentSlot.selected_unit ? (
+        detachmentSize(detachmentSlot.selected_unit) >
+        detachmentSlot.selected_unit.max_size ? (
           <p className="text-center text-red-500 sm:text-xl">
             Detachment is to large!
           </p>
@@ -218,7 +224,9 @@ const BuilderDetachment = ({
       {/* SELECT SECTION */}
       <div className="px-1 pb-2">
         <select
-          value={slot.selected_unit ? slot.selected_unit.id : 0}
+          value={
+            detachmentSlot.selected_unit ? detachmentSlot.selected_unit.id : 0
+          }
           onChange={(e) => changeDetachment(Number(e.target.value))}
           className="w-full text-center my-2 py-1 border border-green-950 font-graduate "
         >
@@ -227,20 +235,22 @@ const BuilderDetachment = ({
         </select>
 
         {/* UNIT UPGRADE SELECTIONS */}
-        {slot.selected_unit ? (
-          slot.selected_unit.upgrade_options ? (
+        {detachmentSlot.selected_unit ? (
+          detachmentSlot.selected_unit.upgrade_options ? (
             <div className="flex flex-col gap-1">
-              {slot.selected_unit.upgrade_options.map((option, index) => (
-                <BuilderUnitUpgradeSelect
-                  key={slot.slot_ref + "upgrades" + index}
-                  unitId={slot.selected_unit!.id}
-                  upgradeOption={option}
-                  setArmyList={setArmyList}
-                  slotSet={slotSet}
-                  refId={slot.ref_id}
-                  slotRef={slot.slot_ref}
-                />
-              ))}
+              {detachmentSlot.selected_unit.upgrade_options.map(
+                (option, index) => (
+                  <BuilderUnitUpgradeSelect
+                    key={detachmentSlot.slot_ref + "upgrades" + index}
+                    unitId={detachmentSlot.selected_unit!.id}
+                    upgradeOption={option}
+                    setArmyList={setArmyList}
+                    slotSet={slotSet}
+                    refId={detachmentSlot.ref_id}
+                    slotRef={detachmentSlot.slot_ref}
+                  />
+                )
+              )}
             </div>
           ) : null
         ) : null}
