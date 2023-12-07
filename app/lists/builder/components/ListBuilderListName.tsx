67314f4b47ab93b409558d2968ccd7e12a05b521
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-12-07 17:40:04 +0000

    fixes

--- app/lists/builder/components/ListBuilderListName.tsx
+++ app/lists/builder/components/ListBuilderListName.tsx
@@ -5,16 +5,14 @@ import { ImQuill } from "@react-icons/all-files/im/ImQuill";
 const ListBuilderListName = () => {
   const { list, setList } = listState();
   return (
-    <div className="text-green-950 font-semibold flex gap-2 justify-center border border-green-950 items-center py-2 px-4 rounded-xl max-w-full">
-      <ImQuill />
+    <div className="text-green-950 font-semibold flex gap-2 justify-center border border-green-950 items-center py-2 px-4 rounded-xl max-w-full mx-8">
       <input
         type="text"
         maxLength={25}
         value={list.list_name}
         onChange={(e) => setList({ ...list, list_name: e.target.value })}
-        className="bg-inherit text-center font-graduate text-lg sm:text-xl max-w-[80%] truncate"
+        className="bg-inherit text-center font-graduate text-lg sm:text-xl truncate"
       />
-      <ImQuill />
     </div>
   );
 };
