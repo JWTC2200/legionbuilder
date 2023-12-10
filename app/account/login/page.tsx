271a0847adade2bcbeaf4acf9282cd6bcf29accf
Kirk Bushell <torm3nt@gmail.com>
2023-12-10 21:01:25 +1100

    Got styling working with prettier and eslint.

--- app/account/login/page.tsx
+++ app/account/login/page.tsx
@@ -1,17 +1,17 @@
-'use client'
+"use client"
 
-import React, {useState} from 'react'
-import Link from 'next/link'
-import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
-import {useRouter} from 'next/navigation'
-import useAuthState from '@/app/Auth'
-import Main from '@components/Main'
-import {BreadCrumbs, Crumb} from '@components/BreadCrumbs'
+import React, { useState } from "react"
+import Link from "next/link"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import { useRouter } from "next/navigation"
+import useAuthState from "@/app/Auth"
+import Main from "@components/Main"
+import { BreadCrumbs, Crumb } from "@components/BreadCrumbs"
 
 const page = () => {
-	const [password, setPassword] = useState('')
-	const [email, setEmail] = useState('')
-	const [error, setError] = useState<string>('')
+	const [password, setPassword] = useState("")
+	const [email, setEmail] = useState("")
+	const [error, setError] = useState<string>("")
 	const saveSession = useAuthState((state) => state.saveSession)
 
 	const auth = getAuth()
@@ -21,15 +21,15 @@ const page = () => {
 		e.preventDefault()
 
 		if (!email) {
-			setError('Email required!')
+			setError("Email required!")
 		}
 
 		if (!password) {
-			setError('Password required!')
+			setError("Password required!")
 			return
 		}
 
-		setError('')
+		setError("")
 
 		try {
 			const credentials = await signInWithEmailAndPassword(
@@ -41,40 +41,63 @@ const page = () => {
 			router.back()
 		} catch (error) {
 			console.log(error)
-			setError('Login failed')
+			setError("Login failed")
 		}
 	}
 
 	return (
 		<Main className="flex flex-col gap-6">
+			{" "}
 			<BreadCrumbs>
-				<Crumb href="/account">Account</Crumb>
-				<Crumb href="/account/login">Login</Crumb>
+				{" "}
+				<Crumb href="/account">Account</Crumb>{" "}
+				<Crumb href="/account/login">Login</Crumb>{" "}
 			</BreadCrumbs>
-
 			{error ? <div className="text-red-500 px-4">{error}</div> : null}
-
 			<section className="flex flex-col gap-12 p-4 w-full lg:w-1/2">
-				<form onSubmit={handleSignIn} className="flex flex-col gap-6 items-start">
+				<form
+					onSubmit={handleSignIn}
+					className="flex flex-col gap-6 items-start"
+				>
 					<div className="flex items-center w-full">
-						<label htmlFor="email" className="font-graduate w-1/4">Email</label>
-						<input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="text-secondary-200 bg-secondary-700 w-3/4 p-1 px-2"/>
+						<label htmlFor="email" className="font-graduate w-1/4">
+							Email
+						</label>{" "}
+						<input
+							type="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							className="text-secondary-200 bg-secondary-700 w-3/4 p-1 px-2"
+						/>
 					</div>
 					<div className="flex items-center w-full">
-						<label htmlFor="password" className="font-graduate w-1/4">Password</label>
-						<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="text-secondary-200 bg-secondary-700 w-3/4 p-1 px-2"/>
+						<label
+							htmlFor="password"
+							className="font-graduate w-1/4"
+						>
+							Password
+						</label>{" "}
+						<input
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							className="text-secondary-200 bg-secondary-700 w-3/4 p-1 px-2"
+						/>
 					</div>
 					<div className="w-full">
-						<button type="submit" className="bg-primary-500 clip-path-halfagon-sm py-1 px-4 text-primary-100 font-semibold font-graduate hover:bg-primary-500 hover:text-primary-500 ml-1/4">
+						<button
+							type="submit"
+							className="bg-primary-500 clip-path-halfagon-sm py-1 px-4 text-primary-100 font-semibold font-graduate hover:bg-primary-500 hover:text-primary-500 ml-1/4"
+						>
 							Login
 						</button>
 					</div>
 				</form>
 
 				<p className="p-4 bg-secondary-800 text-secondary-300 clip-path-octagon-md italic">
-					Please note: An account is NOT required to use the builder. An account is only required if you want
-					to save
-					more than one list or create links for sharing with other players.
+					Please note: An account is NOT required to use the builder.
+					An account is only required if you want to save more than
+					one list or create links for sharing with other players.
 				</p>
 			</section>
 		</Main>
