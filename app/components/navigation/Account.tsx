67bdb1ef4aa613a89c902b431c507a4f0c24f846
Kirk Bushell <torm3nt@gmail.com>
2023-12-11 22:02:12 +1100

    Switched CSS subnav toggling to JS.

--- app/components/navigation/Account.tsx
+++ app/components/navigation/Account.tsx
@@ -1,11 +1,15 @@
 import { getAuth, signOut } from "firebase/auth";
 import { AccountCircle, Logout } from "@components/Icons";
+import { useState } from "react";
 import NavLink from "./NavLink";
 import NavItem from "./NavItem";
 import useAuthState from "@/app/Auth";
 
 export default function () {
   const reset = useAuthState((state) => state.reset);
+  const [visible, setVisible] = useState(false);
+  const toggleAccountMenu = () =>
+    visible ? setVisible(false) : setVisible(true);
 
   const logout = async () => {
     await signOut(getAuth());
@@ -13,13 +17,21 @@ export default function () {
   };
 
   return (
-    <NavItem className="group flex flex-col">
-      <button className="w-full flex gap-4 lg:gap-2 items-center text-primary-400 hover:text-primary-100">
+    <NavItem className="flex flex-col">
+      <button
+        className="w-full flex gap-4 lg:gap-2 items-center text-primary-400 hover:text-primary-100"
+        onClick={toggleAccountMenu}
+      >
         <AccountCircle className={"w-6 h-6 lg:w-4 lg:h-4"} />
         <span>Account</span>
       </button>
       <nav className={"relative"}>
-        <div className={"absolute h-0 group-focus-within:h-40 overflow-hidden transition-all duration-200 ml-3 lg:-ml-3"}>
+        <div
+          className={`
+            absolute overflow-hidden transition-all duration-200 ml-3 lg:-ml-3 ${
+              visible ? "h-40" : "h-0"
+            }`}
+        >
           <ol className="flex flex-col gap-6 lg:bg-primary-950 p-6 lg:px-3">
             <NavItem>
               <NavLink path={"/account"} icon={"manageAccount"}>
@@ -27,7 +39,10 @@ export default function () {
               </NavLink>
             </NavItem>
             <NavItem>
-              <button onClick={logout} className="flex items-center gap-2 hover:text-tertiary-700 active:text-tertiary-600">
+              <button
+                onClick={logout}
+                className="flex items-center gap-2 hover:text-tertiary-700 active:text-tertiary-600"
+              >
                 <Logout className={"w-6 h-6 lg:w-4 lg:h-4"} />
                 <span>Logout</span>
               </button>
