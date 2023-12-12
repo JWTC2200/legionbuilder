8f69770c6442a9e9024f5f367e399568f29cd6dd
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-12-12 17:28:58 +0000

    styling

--- app/reference/calculator/[components]/CalculatorResults.tsx
+++ app/reference/calculator/[components]/CalculatorResults.tsx
@@ -8,17 +8,16 @@ const CalculatorResults = () => {
 	const { calcTargets } = calculatorTarget()
 
 	return (
-		<section className="w-full text-secondary-300 my-4">
-			<header className="w-full bg-backgrounds-950 text-center py-4 font-graduate text-xl font-semibold">RESULTS</header>
+		<section className="w-full text-secondary-300 my-2">
 			<div className="flex flex-col md:flex-row gap-4 sm:justify-center items-center sm:items-start py-4">
 				{calcWeapons.length || calcUnit ? (
 					<div className="w-full">
 						<AttackDisplay />
 					</div>
 				) : (
-					<div className="text-center text-red-600 w-full">Select a weapon or attacking unit</div>
+					<div className="text-center text-red-600 w-full font-graduate text-secondary-100">Select a weapon or attacking unit</div>
 				)}
-				{calcTargets ? <TargetDisplay /> : <div className="text-center text-red-600 w-full">Select a target unit</div>}
+				{calcTargets ? <TargetDisplay /> : <div className="text-center text-red-600 w-full font-graduate text-secondary-100">Select a target unit</div>}
 			</div>
 		</section>
 	)
