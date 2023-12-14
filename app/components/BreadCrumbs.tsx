f83ae1da8de6638da453a8828fd2c8ea4ab512ec
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-12-14 16:37:00 +0000

    removed some anys

--- app/components/BreadCrumbs.tsx
+++ app/components/BreadCrumbs.tsx
@@ -1,23 +1,31 @@
 "use client"
 
-import { Children, ReactNode, useEffect, useRef, useState } from "react"
+import { Children, ReactNode, isValidElement, useEffect, useRef, useState } from "react"
 import { usePathname } from "next/navigation"
 import Link from "next/link"
 import { CaretDown, CaretUp } from "@components/Icons"
 import { clickOutside } from "@/app/utils/events"
 
 export function BreadCrumbs({ children }: { children: ReactNode }) {
-	children = Children.toArray(children)
+	// children = Children.toArray(children)
 
 	return (
 		<header className="bg-secondary-900 p-4 py-2 text-lg font-graduate">
-			{/* @ts-ignore - you can actually map over this, so not sure what TS is bitching about! */}
-			{children.map((child: any, key: string) => (
+			{/* ts-ignore - you can actually map over this, so not sure what TS is bitching about! */}
+			{/* {children.map((child: any, key: string) => (
 				<span className="group" key={key}>
 					{child}
 					<span className="group-last:hidden px-2 text-secondary-500">&gt;</span>
 				</span>
-			))}
+			))} */}
+			{Children.map(children, (child, key) =>
+				isValidElement(child) ? (
+					<span className="group" key={key}>
+						{child}
+						<span className="group-last:hidden px-2 text-secondary-500">&gt;</span>
+					</span>
+				) : null
+			)}
 		</header>
 	)
 }
--- app/components/Sticky.tsx
+++ app/components/Sticky.tsx
@@ -1,19 +1,18 @@
-import {ReactNode, useEffect, useRef, useState} from "react";
+import { ReactNode, useEffect, useRef, useState } from "react"
 
-export default function({ className, children } : { className?: string, children: ReactNode }) {
-    const ref = useRef(null)
-    const [position, setPosition] = useState(0)
+export default function ({ className, children }: { className?: string; children: ReactNode }) {
+	const ref = useRef<HTMLDivElement>(null)
+	const [position, setPosition] = useState(0)
 
-    useEffect(() => {
-        if (ref == null) return;
-        
-        // @ts-ignore - couldn't get ref.current to stop whining. It'll never be null at this point, due to conditional check above ^
-        setPosition(ref.current.getBoundingClientRect().y);
-    })
-    
-    return (
-        <section className={`sticky ${className}`} ref={ref} style={{ top: position +'px' }}>
-            {children}
-        </section>
-    );
+	useEffect(() => {
+		if (ref !== null && ref.current) {
+			setPosition(ref.current.getBoundingClientRect().y)
+		}
+	})
+
+	return (
+		<section className={`sticky ${className}`} ref={ref} style={{ top: position + "px" }}>
+			{children}
+		</section>
+	)
 }
