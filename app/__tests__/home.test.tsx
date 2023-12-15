6c391cb306057a715cb042781996f4edc105a7bb
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2023-12-15 20:33:57 +0000

    15/12

--- app/__tests__/home.test.tsx
+++ app/__tests__/home.test.tsx
@@ -2,7 +2,10 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import SpecialRuleBox from "../components/SpecialRuleBox"
-import Home from "../page"
+
+jest.mock("nanoid", () => {
+	return { nanoid: () => "123" }
+})
 
 test("SpecialRuleBox Render", () => {
 	render(<SpecialRuleBox rule={{ name: "Armoured" }} />)
@@ -11,4 +14,7 @@ test("SpecialRuleBox Render", () => {
 	render(<SpecialRuleBox rule={{ name: "Jink", value: 5 }} />)
 	expect(screen.getByText("Jink (5)"))
 	expect(screen.getByText("Main Rulebook p.92"))
+
+	render(<SpecialRuleBox rule={{ name: "Made up rule" }} />)
+	expect(screen.queryByTestId("special_rule")).toBeNull
 })
