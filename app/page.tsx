7322bbf6d57264b50a296ce67a447d81fdd41bf2
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2024-01-09 13:26:10 +0000

    replace discord link

--- app/page.tsx
+++ app/page.tsx
@@ -13,20 +13,20 @@ export default function Home() {
 
 			<section className="mx-4">
 				<h2 className="text-lg font-graduate text-tertiary-700 mt-4">Suggestions or feedback?</h2>
-				<Link href="https://discord.gg/UE3RzpKK" target="_blank" className="hover:underline text-xl flex items-center gap-1 hover:text-tertiary-700 active:text-tertiary-700 focus:text-tertiary-700">
+				<Link href="https://discord.gg/KhQeqBn8" target="_blank" className="hover:underline text-xl flex items-center gap-1 hover:text-tertiary-700 active:text-tertiary-700 focus:text-tertiary-700 mt-2">
 					Join our Discord! <FaDiscord />
 				</Link>
-				<Link href="/contact" className="hover:underline text-xl flex items-end gap-1 hover:text-tertiary-700 active:text-tertiary-700 focus:text-tertiary-700">
+				<Link href="/contact" className="hover:underline text-xl flex items-end gap-1 hover:text-tertiary-700 active:text-tertiary-700 focus:text-tertiary-700 mt-2">
 					Contact me <TfiEmail />
 				</Link>
 				<Link
 					href="https://github.com/JWTC2200/legionbuilder"
 					className="mb-2 text-xl flex items-center gap-1
-        hover:text-tertiary-700 hover:underline active:text-tertiary-700 focus:text-tertiary-700">
+        hover:text-tertiary-700 hover:underline active:text-tertiary-700 focus:text-tertiary-700 mt-2">
 					Github <FaGithub />
 				</Link>
 			</section>
-			<hr className="border rounded-full bg-stone-900 m-4 " />
+			<hr className="border rounded-full bg-stone-900 m-4" />
 			<footer className="mx-4">
 				<p>This website is unofficial and in no way endorsed by Games Workshop.</p>
 				<p>Any use of terms from Games Workshop are used without permission. No challenge to their status indended. All rights reserved to their respective owners.</p>
