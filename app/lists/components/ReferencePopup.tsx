fe6173e2d678937e1bc17367c536ad73090aa918
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2024-01-26 16:13:56 +0000

    update ReferencePopup

--- app/lists/components/ReferencePopup.tsx
+++ app/lists/components/ReferencePopup.tsx
@@ -12,16 +12,23 @@ interface properties {
 
 const ReferencePopup = ({ detachmentId, children }: properties) => {
 	const [hidden, setHidden] = useState(true)
-	const Icon = Icons["information"]
+	// const Icon = Icons["information"]
+	const Icon = (
+		<div className="rounded-full h-5 w-5 my-1 flex items-center justify-center bg-backgrounds-900 text-tertiary-50 hover:bg-backgrounds-800 hover:text-tertiary-400 font-bold">
+			?
+		</div>
+	)
 
 	const unit = detachmentData.find((detachment) => detachment.id === detachmentId)!
 
 	return (
-		<button className="hover:text-tertiary-800 active:text-tertiary-800 cursor-help select-text" onClick={() => setHidden((prev) => !prev)}>
-			{children ? children : <Icon className="" />}
+		<button
+			className="hover:text-tertiary-800 active:text-tertiary-800 cursor-help select-text"
+			onClick={() => setHidden((prev) => !prev)}>
+			{children ? children : Icon}
 			{unit && !hidden ? (
-				<div className="absolute max-w-xl top-0 left-0 z-50 rounded-lg bg-dataslate">
-					<div className=" text-primary-950 font-semibold hover:text-tertiary-800 pt-1">Click to close</div>
+				<div className="absolute max-w-xl top-0 left-0 z-50 rounded-lg bg-backgrounds-950 mt-1 px-2 pb-2">
+					<div className="font-semibold hover:text-tertiary-800 text-primary-50 py-2">Click to close</div>
 					<DetachmentDataslate detachment={unit} no_related={true} />
 				</div>
 			) : null}
