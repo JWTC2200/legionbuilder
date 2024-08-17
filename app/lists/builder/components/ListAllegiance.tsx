49da14fcdf6a8da370c162a59ba6745d6c52c423
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2024-08-17 16:28:12 +0100

    filted formations by list alliance, deletes formations that aren't neutral or same allegiance

--- app/lists/builder/components/ListAllegiance.tsx
+++ app/lists/builder/components/ListAllegiance.tsx
@@ -1,8 +1,28 @@
 import { listState } from "@/app/lists/state"
 import { ALLEGIANCE } from "@type/types"
+import { removeFormation } from "@lists/builder/utils"
+import { List, ListFormation } from "@type/listTypes"
+import { all } from "deepmerge"
 
 const ListAllegiance = () => {
 	const { list, setList } = listState()
+
+	const changeAllegiance = (allegiance: ALLEGIANCE) => {
+		const invalidFormations = list.formations.filter((formation) => {
+			if (formation.allegiance !== null && formation.allegiance !== allegiance) return formation
+		})
+
+		const removeInvalidFormations = (list: List, formations: ListFormation[], count: number): List => {
+			if (count >= formations.length) return list
+
+			return removeInvalidFormations(removeFormation(list, formations[count]), formations, count + 1)
+		}
+
+		console.log(invalidFormations.map((formation) => formation.name))
+
+		setList({ ...removeInvalidFormations(list, invalidFormations, 0), allegiance: allegiance })
+	}
+
 	return (
 		<div className="flex items-center">
 			<label htmlFor="faction_selector" className="text-lg sm:text-xl font-graduate mr-1">
@@ -12,7 +32,7 @@ const ListAllegiance = () => {
 				id="allegiance_selector"
 				name="allegiance_selector"
 				value={list.allegiance}
-				onChange={(e) => setList({ ...list, allegiance: e.target.value as ALLEGIANCE })}
+				onChange={(e) => changeAllegiance(e.target.value as ALLEGIANCE)}
 				className="bg-transparent rounded-sm p-1 sm:text-lg font-graduate hover:text-primary-400 active:text-primary-400">
 				<option value={ALLEGIANCE.loyalist} className="text-primary-950">
 					{ALLEGIANCE.loyalist}
