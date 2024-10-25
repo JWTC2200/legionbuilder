ffe3b662bab00d263f863ca9ffd0015ae71ef7a4
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2024-10-25 10:13:03 +0100

    Fixed an issue with checkUploadPermission using the wrong object key

--- app/firebase/firestore/checkUploadPermission.ts
+++ app/firebase/firestore/checkUploadPermission.ts
@@ -6,11 +6,8 @@ export const checkUploadPermission = async (listData: List) => {
 	const q = query(collection(db, process.env.NEXT_PUBLIC_FIREBASE_LIST_DB!), where("owner", "==", listData.user))
 	const data: string[] = []
 	const querySnapshot = await getDocs(q)
-	querySnapshot.forEach((doc) => data.push(JSON.parse(doc.data().list).list_id))
-	if (data.includes(listData.id) || data.length < 10) {
-		return true
-	}
-	return false
+	querySnapshot.forEach((doc) => data.push(JSON.parse(doc.data().list).id))
+	return data.includes(listData.id) || data.length < 20
 }
 
 export default checkUploadPermission
