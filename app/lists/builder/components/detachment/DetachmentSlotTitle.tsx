4920fea0e33b31877ee33fd77086f6a01b98aeb8
JWTC2200 <113539489+JWTC2200@users.noreply.github.com>
2024-11-10 09:05:31 +0000

    Fixes
    DetachmentSlotTitle.tsx icons not changed

--- app/lists/builder/components/detachment/DetachmentSlotTitle.tsx
+++ app/lists/builder/components/detachment/DetachmentSlotTitle.tsx
@@ -1,16 +1,17 @@
 import { DETACHMENT_TYPE } from "@type/types"
 import { getDetachmentIcon } from "@/app/utils/detachmentIcons"
+import Icons from "@components/Icons"
 
 interface properties {
 	slotType: DETACHMENT_TYPE
 }
 
 const DetachmentSlotTitle = ({ slotType }: properties) => {
-	const detachmentIcon = getDetachmentIcon(slotType)
+	const Icon = Icons[getDetachmentIcon(slotType)]
 	return (
-		<h4 className="sm:w-full flex gap-1 items-center justify-center builder_title_background text-primary-100 clip-path-halfagon-md text-center py-2 sm:text-xl font-graduate px-2 mx-2 sm:mx-0">
-			{detachmentIcon} {slotType}
-			{detachmentIcon}
+		<h4 className="sm:w-full flex gap-2 items-center justify-center builder_title_background text-primary-100 clip-path-halfagon-md text-center py-2 sm:text-xl font-graduate px-2 mx-2 sm:mx-0">
+			<Icon className={"sm:h-6 sm:w-6"} /> {slotType}
+			<Icon className={"sm:h-6 sm:w-6"} />
 		</h4>
 	)
 }
